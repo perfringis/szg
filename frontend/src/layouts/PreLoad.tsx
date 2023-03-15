08ce4bb285eb835973ab5af5a7a323234f5b37c0
perfringis <kopko.mateusz@gmail.com>
2023-03-15 15:44:12 +0100

    implement pre load page

--- frontend/src/layouts/PreLoad.tsx
+++ frontend/src/layouts/PreLoad.tsx
@@ -1,6 +1,32 @@
-
 export default function PreLoad() {
   return (
-    <></>
-  )
+    <>
+    <div id="pre-load-page" className="flex flex-col h-screen bg-[#050505]">
+      <div
+        id="menu"
+        className="flex flex-row px-12 justify-between items-center bg-[#171717] h-20"
+      >
+        <div id="left-menu" className="flex flex-row items-center gap-12">
+          <div id="brand" className="flex flex-row items-center gap-2">
+            <div id="logo" className="h-5 w-4 bg-[#E16657] rounded-md"></div>
+            <div id="name" className="h-3 w-14 bg-white rounded-md"></div>
+          </div>
+          <div className="h-3 w-14 bg-[#1F1F1F] rounded-md"></div>
+          <div className="h-3 w-14 bg-[#1F1F1F] rounded-md"></div>
+        </div>
+        <div id="right-menu" className="flex flex-row items-center gap-8">
+          <div className="h-5 w-5 bg-[#1F1F1F] rounded-md"></div>
+          <div className="h-3 w-4 bg-[#1F1F1F] rounded-md"></div>
+        </div>
+      </div>
+      <div id="main" className="flex flex-col bg-[#050505] items-center gap-6 py-60">
+        <div id="brand" className="flex flex-row items-center gap-6">
+          <div id="logo" className="h-16 w-14 bg-[#E16657] rounded-md"></div>
+          <div id="name" className="h-8 w-44 bg-white rounded-md"></div>
+        </div>
+        <div id="search" className="h-16 w-px-704 bg-[#181818] rounded-md"></div>
+      </div>
+    </div>
+    </>
+  );
 }
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'px-704': "704px",
+      },
+    },
   },
   plugins: [require("@headlessui/tailwindcss")({ prefix: "ui" })],
 };
